b7f9152d4f6f044abbe23df1b4973171ae1ad15c
AldiState <aldistate@yahoo.com>
2023-11-07 10:55:43 +0700

    create form ParticipantsForm

---
+++ resources/js/Pages/Form/ParticipantsForm.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function ParticipantsForm() {
+    return (
+        <>
+            <div>ParticipantsForm</div>
+        </>
+    );
+}
+
+export default ParticipantsForm;
