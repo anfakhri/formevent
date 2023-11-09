77e5b1fc526bb8af95ced0488fcad3a2c4e423ad
A-Fakhr1 <abdunnashirfakhri@gmail.com>
2023-11-09 16:45:54 +0700

    part controller

--- app/Http/Controllers/ParticipantController.php
+++ app/Http/Controllers/ParticipantController.php
@@ -35,7 +35,10 @@ class ParticipantController extends Controller
     public function store(ParticipantCreateRequest $request)
     {
         //
-        $participant = Participant::create($request->all());
+        $validated = $request->validated();
+        Participant::create($validated);
+
+        return back()->with('success','Data has been Saved Successfully');
     }
 
     /**
